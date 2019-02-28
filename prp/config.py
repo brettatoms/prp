3928f192151bf9ead15363c899879bdd2b3b064c
Brett Adams <brettadams@fastmail.com>
2019-02-27 23:02:29 -0500

    Initial commit

---
+++ prp/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+from pathlib import Path
+import toml
+
+
+@lru_cache()
+def _load_pyproject_toml():
+    pyproject_path = Path(Path.cwd(), "pyproject.toml")
+    with open(pyproject_path, 'r') as f:
+        d = toml.load(f)
+        return d.get('tool', {}).get('prp', {})
+
+
+def get(key, default=None):
+    config = _load_pyproject_toml()
+    return config.get(key, default)
